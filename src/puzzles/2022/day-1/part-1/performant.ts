2f44b3068f80a432490f88fedbf8c3a199e925da
Miles <miles@karmacheck.com>
2022-12-02 14:39:43 -0600

    a

--- src/puzzles/2022/day-1/part-1/performant.ts
+++ src/puzzles/2022/day-1/part-1/performant.ts
@@ -4,15 +4,18 @@ export default (input: string[]) => {
     let max = 0;
     let currentLoad = 0;
     input.forEach((line) => {
-        if (line === '') {
+        if (line === "") {
             if (currentLoad > max) {
                 max = currentLoad;
             }
             currentLoad = 0;
         } else {
-            currentLoad += parseInt(line);
+            const food = parseInt(line);
+            currentLoad += food;
         }
     })
+
+
     return max;
 }
 
